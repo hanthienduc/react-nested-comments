a1cf7500d89146e0341cc1666d2f7f2a3e60bccf
hanthienduc <hanthienduc.96@gmail.com>
2022-08-15 11:44:07 +0900

    add custom hook for making an asynchronos request to server

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useAsync } from "../hooks/useAsync";
 import { Post } from "../interfaces/Post";
 import { getPosts } from "../service/post";
 
 export function PostList() {
 
-  const [posts, setPosts] = useState<Post[]>([])
+  const { loading, error, value: posts } = useAsync<Post[]>(getPosts)
 
-  useEffect(() => {
-    async function getAllPosts() {
-      const posts = await getPosts()
-      setPosts(posts)
-    }
-    getAllPosts()
-  }, [])
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1 className="error-msg">{error}</h1>
 
   return (
     <>
       <h1>Post List</h1>
-      <ul>
-        {posts?.map(post => (<li key={post.id}><Link to={post.id}>{post.title}</Link></li>))}
-      </ul>
+      {posts?.map(post =>
+        (<h1 key={post.id}><Link to={post.id}>{post.title}</Link></h1>)
+      )}
     </>
   )
 }
---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export function useAsync<Type>(func: () => Promise<any>, dependencies?: []) {
+  const { execute, ...state } = useAsyncInternal<Type>(func, dependencies, true)
+  useEffect(() => {
+    execute()
+  }, [execute])
+  return state
+}
+
+export function useAsyncFn<Type>(func: () => Promise<any>, dependencies: []) {
+  return useAsyncInternal<Type>(func, dependencies, false)
+}
+
+export function useAsyncInternal<Type>(
+  func: (...params: any) => Promise<any>,
+  dependencies?: [],
+  initialLoading = false
+) {
+  const [loading, setLoading] = useState(initialLoading)
+  const [error, setError] = useState()
+  const [value, setValue] = useState<Type>()
+
+  const execute = useCallback((...params: any) => {
+    setLoading(true)
+    return func(...params)
+      .then((data: any) => {
+        setValue(data)
+        setError(undefined)
+        return data
+      })
+      .catch((error: any) => {
+        setError(error)
+        setValue(undefined)
+        return Promise.reject(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, dependencies ?? [])
+
+  return { loading, error, value, execute }
+}
