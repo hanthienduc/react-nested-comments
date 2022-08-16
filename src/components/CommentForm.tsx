bedfa850054098be57a2d64ece945cfdb84e9a8b
hanthienduc <hanthienduc.96@gmail.com>
2022-08-16 10:29:52 +0900

    add comment form and display comment component

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -2,27 +2,30 @@ import { SetStateAction, SyntheticEvent, useState } from "react"
 
 interface CommentFormProps {
   loading: boolean,
-  error: string,
+  error: string | undefined,
   onSubmit: (message: string) => Promise<any>
-  autoFocus: boolean
-  initialValue: ''
+  autoFocus?: boolean
+  initialValue?: ''
 }
 
-export function CommentForm({ loading, error, onSubmit, autoFocus, initialValue = '' }: CommentFormProps) {
+export function CommentForm({ loading, error, onSubmit, autoFocus = false, initialValue = '' }: CommentFormProps) {
 
   const [message, setMessage] = useState<string>(initialValue)
 
   function handleSubmit(e: SyntheticEvent) {
     e.preventDefault()
+    onSubmit(message).then(() => setMessage(''))
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <textarea className='message-input' value={message} autoFocus={autoFocus}
-        onChange={(e) => setMessage(e.target.value as SetStateAction<string>)
-        }
-      />
-      <button disabled={loading} className="btn">{loading ? 'Loading...' : 'Post'}</button>
+      <div className="comment-form-row">
+        <textarea className='message-input' value={message} autoFocus={autoFocus}
+          onChange={(e) => setMessage(e.target.value as SetStateAction<string>)
+          }
+        />
+        <button disabled={loading} className="btn">{loading ? 'Loading...' : 'Post'}</button>
+      </div>
       <div className="error-msg">
         {error}
       </div>
--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,14 +1,27 @@
 import { Comment } from "../types/PostContextType";
+import { IconBtn } from "./IconBtn";
 
 interface CommentProps {
   comment: Comment
 }
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: 'medium',
+  timeStyle: 'short'
+})
+
 export function CommentItem({ comment }: CommentProps) {
   const { id, message, user, createdAt, likeCount, likedByMe } = comment
   return (
     <div className="comment">
+      <div className="header">
         <span className="name">{user.name}</span>
+        <span className="date">{dateFormatter.format(Date.parse(createdAt))}</span>
+      </div>
+      <div className="message">{message}</div>
 
+      <div className="footer">
+          
+      </div>
     </div>
   )
 }
